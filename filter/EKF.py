94ad610c6d3cfd37ef832d43e6223dfc3768d066
OctopusNO1 <1031389703@qq.com>
2018-08-31 20:44:25 +0800

    .idea is configuration

---
+++ filter/EKF.py
@@ -0,0 +1,20 @@
+from math import tan, sin, cos, sqrt
+
+
+def predict(x, dt, u, wheelbase):
+    hdg = x[2]
+    vel = u[0]
+    steering_angle = u[1]
+    dist = vel * dt
+
+    if abs(steering_angle) > 0.001: # is robot turning?
+        beta = (dist / wheelbase) * tan(steering_angle)
+        r = wheelbase / tan(steering_angle) # radius
+
+        sinh, sinhb = sin(hdg), sin(hdg + beta)
+        cosh, coshb = cos(hdg), cos(hdg + beta)
+        return x + np.array([-r*sinh + r*sinhb, 
+                              r*cosh - r*coshb, beta])
+    else:   # moving in straight line
+        return x + np.array([dist*cos(hdg), dist*sin(hdg), 0])
+
